3956563d269292b7aafa2d57c43d892c8912943b
AhmedRedaMansi <121058536+AhmedRedaMansi@users.noreply.github.com>
2022-12-19 22:17:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter a number ");
+int num=int.Parse(Console.ReadLine());
+for(int i=2;i<num;i++)
+{
+
+    if (num %i==0)
+        
+        Console.WriteLine("this number not prime");
+    return;
+}
+Console.WriteLine("this number prime");
